7ff0650134670fbd18e09038406461204974d969
Ilya Volf <volfilya@gmail.com>
2023-01-29 19:48:26 +0700

    screens added

--- app/src/main/java/com/thesis/sportologia/MainActivity.kt
+++ app/src/main/java/com/thesis/sportologia/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.thesis.sportologia
 
+import android.graphics.Typeface
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.thesis.sportologia.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,6 +16,14 @@ class MainActivity : AppCompatActivity() {
 
         val binding = ActivityMainBinding.inflate(layoutInflater).also { setContentView(it.root) }
 
+        /*val tf = Typeface.createFromAsset(
+            assets,
+            "fonts/SourceSansPro/SourceSansPro-Bold.ttf"
+        )
+        val tv1 = findViewById<View>(R.id.title) as TextView
+        val tv2 = findViewById<View>(R.id.account_type) as TextView
+        tv1.typeface = tf
+        tv2.typeface = tf*/
 
     }
 }
