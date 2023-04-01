ad425da89d9afeed2922ba4ec581e95c44793513
Ilya Volf <volfilya@gmail.com>
2023-04-01 23:25:31 +0700

    list photos started

--- app/src/main/java/com/thesis/sportologia/ui/PhotosFragment.kt
+++ app/src/main/java/com/thesis/sportologia/ui/PhotosFragment.kt
@@ -6,31 +6,60 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
+import androidx.navigation.navOptions
+import androidx.viewpager2.widget.ViewPager2
+import com.thesis.sportologia.R
 import com.thesis.sportologia.databinding.FragmentPhotosBinding
+import com.thesis.sportologia.ui.adapters.PagerAdapter
+import com.thesis.sportologia.ui.users.ListUsersFragmentFollowers
 import com.thesis.sportologia.ui.views.OnToolbarBasicAction
 import dagger.hilt.android.AndroidEntryPoint
 import java.net.URI
 
-@AndroidEntryPoint
+/**@AndroidEntryPoint
 class PhotosFragment : Fragment() {
+    private val args by navArgs<PhotosFragmentArgs>()
+
     private lateinit var binding: FragmentPhotosBinding
+    private lateinit var userId: String
+    private lateinit var adapter: PagerAdapter
+    private lateinit var viewPager: ViewPager2
+
+    private fun getUserId(): String = args.userId
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentPhotosBinding.inflate(inflater, container, false)
+        userId = getUserId()
 
-        binding.toolbar.setListener {
-            if (it == OnToolbarBasicAction.LEFT) {
-                onBackButtonPressed()
-            }
-        }
+        initToolbar()
+        initContentBlock()
 
         return binding.root
+    }
+
+    private fun initToolbar() {
+        binding.photosToolbar.setListener {
+            when (it) {
+                OnToolbarBasicAction.LEFT -> onBackButtonPressed()
+                else -> {}
+            }
+        }
+    }
+
+    private fun initContentBlock() {
+        val listUsersFragmentFollowers = ListPhotosFragment.newInstance(userId)
+
+        adapter = PagerAdapter(this, arrayListOf(listUsersFragmentFollowers))
+        viewPager = binding.pager
+        viewPager.adapter = adapter
     }
 
     private fun onBackButtonPressed() {
         findNavController().navigateUp()
     }
-}
+
+}*/
